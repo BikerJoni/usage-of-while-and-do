14d6c1ebe6e3439714a54664815e562a8b937504
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-06-15 20:52:20 +0500

    hometask 4, gorizontal

--- Program.cs
+++ Program.cs
@@ -13,17 +13,19 @@ do
     lastNumber = Convert.ToInt32(inputLast);
     while (number <= lastNumber)
     {
+
         Console.WriteLine(number + " karra jadvali :");
-        Console.WriteLine($"1 x {number} = {number * 1}");
-        Console.WriteLine($"2 x {number} = {number * 2}");
-        Console.WriteLine($"3 x {number} = {number * 3}");
-        Console.WriteLine($"4 x {number} = {number * 4}");
-        Console.WriteLine($"5 x {number} = {number * 5}");
-        Console.WriteLine($"6 x {number} = {number * 6}");
-        Console.WriteLine($"7 x {number} = {number * 7}");
-        Console.WriteLine($"8 x {number} = {number * 8}");
-        Console.WriteLine($"9 x {number} = {number * 9}");
+        Console.Write($"1 x {number} = {number * 1} ");
+        Console.Write($"2 x {number} = {number * 2} ");
+        Console.Write($"3 x {number} = {number * 3} ");
+        Console.Write($"4 x {number} = {number * 4} ");
+        Console.Write($"5 x {number} = {number * 5} ");
+        Console.Write($"6 x {number} = {number * 6} ");
+        Console.Write($"7 x {number} = {number * 7} ");
+        Console.Write($"8 x {number} = {number * 8} ");
+        Console.Write($"9 x {number} = {number * 9} "); 
         number++;
+        
     }
     Console.WriteLine("Do you want continue? (yes -> no)");
     userInput = Console.ReadLine();
