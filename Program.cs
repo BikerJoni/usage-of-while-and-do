f9b959c8c9acf3247b1e4f144e181960e2988ba9
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-06-15 17:05:59 +0500

    birinchi karra jadvali

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int input = 0;
+while(input <= 9)
+{
+    Console.WriteLine($"9 x {input} = {input * 9}");
+    input++;
+}
